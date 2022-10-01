246ebcc3eafbfad9df0948a17a41fa882a27d302
Sylvia <ndilisylvia@gmail.com>
2022-10-01 09:41:44 +0300

    Completed assignment

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -6,6 +6,13 @@ function MoviesPage({ movies }) {
   return (
     <div>
       <MoviesList movies={movies} />
+      <MoviesList movies={movies} />
+      <Route exact path={match.url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
 }
