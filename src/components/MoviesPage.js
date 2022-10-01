71e5444466a1ffdbfd1731ade96854c8f9e61cf0
Sylvia <ndilisylvia@gmail.com>
2022-10-01 09:58:48 +0300

    completed assignment

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
   return (
